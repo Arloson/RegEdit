e841385a94f56517104b2bbd93ba8ebe20a13181
Sergey <sergeyantonov3083@gmail.com>
2018-04-25 17:24:05 +0300

    read file

--- RegMain.cpp
+++ RegMain.cpp
@@ -2,23 +2,26 @@
 #include <fstream>
 #include <algorithm>
 #include <string>
-
+#include <vector>
 using namespace std;
 
-vector& readFile(string name, vector<string> &v){
+void readFile(string name, vector<string> &v){
 	string dat;
 	ifstream ifile;
 	ifile.open(name, ios_base::in);
 	if (ifile.is_open()!=0)
 	{
 		wcout<<L"Файл открыт"<<endl;
-		ifile.getline(dat);
+		while(getline(ifile, dat)){
+			cout<<dat<<endl;
+			v.push_back(dat);
+		}
+
 
 	}
 
-
-
 	ifile.close();
+
 }
 
 void writeNewReg(vector<string>& v){
@@ -28,11 +31,10 @@ void writeNewReg(vector<string>& v){
 int main(int argc, char** argv){
 	setlocale(0, "");
 	wcout<<L"Тест"<<endl;
-
-
-
-
-
+	std::vector<string> v;
+	v.reserve(100);
+	wcout<<"запуск функции, добавление данных в вектор\n";
+	readFile("C:\\Users\\Sergey\\Desktop\\tt\\parsReg\\reg.reg", v);
 
 
 
